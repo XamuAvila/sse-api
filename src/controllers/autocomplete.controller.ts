0a7f0ec65b8f31aa984ed44d996033d2bbfb2ac7
Yasmin Barcelos <78911764+yxsbx@users.noreply.github.com>
2023-05-06 18:54:11 -0300

    Creating post route

--- src/controllers/autocomplete.controller.ts
+++ src/controllers/autocomplete.controller.ts
@@ -2,19 +2,35 @@ import { Request, Response } from "express";
 import sseHeaders from "../constants/requestConstants";
 import getUniqueId from "../utils";
 
+const clients: Array<{id:string, res: Response}> = [];
+
 const connectAutocomplete = (req: Request, res: Response)=>{
     res.writeHead(200, sseHeaders);
     const sendData:string = `Something`;
     res.write(sendData);
     res.flush();
     const clientId:string = getUniqueId();
+
+    clients.push({
+        id: clientId,
+        res
+    });
+
     console.log(`${clientId} - Connection opened`);
     req.on('close', () => {
         console.log(`${clientId} - Connection closed`);
     });
 }
 
-export default {
-    connectAutocomplete
+const sendEvent = (req:Request, res:Response) => {
+    for(let i=0; i<clients.length; i++){
+        clients[i].res.write(`something`);
+        clients[i].res.flush();
+     }
+}
+
+export {
+    connectAutocomplete,
+    sendEvent
 };
 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,10 @@
 import express, { Application } from 'express';
-import connectAutocomplete from '../controllers/autocomplete.controller';
+import {connectAutocomplete, sendEvent} from '../controllers/autocomplete.controller';
 
 export default function initRoutes(app: Application): void {
     const router = express.Router();
     router.get('/autocomplete', connectAutocomplete);
+    router.post('/autocomplete', sendEvent)
     app.use(router);
 }
 
